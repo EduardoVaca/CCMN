56abb7252cd6c83dfb9cefe06667479b8625216d
EduardoVaca <vacalalo@gmail.com>
2016-02-29 14:31:13 -0600

    Added Admin models of library app

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Book, Author, Category, Wrote, Has
 
-# Register your models here.
+@admin.register(Book)
+class AdminBook(admin.ModelAdmin):
+	list_display = ('name',)
+
+@admin.register(Author)
+class AdminAuthor(admin.ModelAdmin):
+	list_display = ('name',)
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+	list_display = ('name',)
+
+@admin.register(Wrote)
+class AdminWrote(admin.ModelAdmin):
+	list_display = ('author', 'book',)
+
+@admin.register(Has)
+class AdminHas(admin.ModelAdmin):
+	list_display = ('book', 'category',)
