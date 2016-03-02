3c5425714078a07fa128a16e956b11fc5ab3db06
EduardoVaca <vacalalo@gmail.com>
2016-03-02 17:56:23 -0600

    Fixed model for AdminUser and done tests

--- users/models.py
+++ users/models.py
@@ -1,20 +1,17 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User, Group
 
 
 class AdminUser(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL)
 
 	def save(self, *args, **kwargs):
-		"""Assign the admin user the permision to enter to admin_dashboard
+		"""Assign the admin user the group to enter to admin_dashboard
 		"""
-		content_type = ContentType.objects.get_for_model(settings.AUTH_USER_MODEL)
-		admin_permission = Permission.objects.get_or_create(codename='admin_dashboard', 
-															name='Admin Dashboard', 
-															content_type=content_type)[0]
-		self.user.user_permissions.add(admin_permission)
+		admin_group = Group.objects.get_or_create(name='administrator')[0]
+		self.user.groups.add(admin_group)		
 		return super(AdminUser, self).save(*args, **kwargs)
 
 	def __str__(self):
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, Permission
 
-# Create your tests here.
+from .models import AdminUser
+
+class AdminUserTest(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+		self.normal_user = User.objects.create_user('normal', 'normal@gmail.com', 'normal')
+
+	def test_admin_user_group(self):
+		"""Tests if the group for admin dashboard is assigned correctly
+		"""
+		admin_user = AdminUser.objects.create(user=self.user)
+		self.assertIsNotNone(admin_user.user.groups.get(name='administrator'))
+		self.assertEqual(str(admin_user), 'temporary')
+
+
+
