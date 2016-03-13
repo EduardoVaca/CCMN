564fa23922f1299c4fac210b08a9687e3f805fb9
EduardoVaca <vacalalo@gmail.com>
2016-03-13 12:15:12 -0600

    Added model for Base User

--- users/models.py
+++ users/models.py
@@ -21,4 +21,36 @@ class AdminUser(models.Model):
 		verbose_name_plural = 'AdminUsers'
 
 
+class BaseUser(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL)
+	name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	address = models.CharField(max_length=255)
+	phone = models.CharField(max_length=10)
+	age = models.IntegerField(null=False)
+
+	SEX_MALE = 'M'
+	SEX_FEMALE = 'F'
+
+	sex_choices = (
+			(SEX_MALE, 'Hombre'),
+			(SEX_FEMALE, 'Mujer')
+		)
+	sex = models.CharField(max_length=1,
+							choices=sex_choices)
+
+	def save(self, *args, **kwargs):
+		"""Assign user into BaseUser group
+		"""
+		base_group = Group.objects.get_or_create(name='usuario_base')[0]
+		self.user.groups.add(base_group)
+		return super(BaseUser, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return self.user.username
+
+	class Meta:
+		verbose_name_plural = 'BaseUsers'
+
+
 
--- users/tests.py
+++ users/tests.py
@@ -1,12 +1,20 @@
 from django.test import TestCase
 from django.contrib.auth.models import User, Permission
 
-from .models import AdminUser
+from .models import AdminUser, BaseUser
 
 class AdminUserTest(TestCase):
 	def setUp(self):
 		self.user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
 		self.normal_user = User.objects.create_user('normal', 'normal@gmail.com', 'normal')
+		self.normal_user_2 = User.objects.create_user('user@gmail.com', 'user@gmail.com', 'user')
+		self.base_user = BaseUser.objects.create(user=self.normal_user_2,
+													name='Eduardo',
+													last_name='Vaca',
+													address='San Salvador 15 San Gil, qro.',
+													age=20,
+													phone='4271171443',
+													sex='M')
 
 	def test_admin_user_group(self):
 		"""Tests if the group for admin dashboard is assigned correctly
@@ -14,6 +22,12 @@ class AdminUserTest(TestCase):
 		admin_user = AdminUser.objects.create(user=self.user)
 		self.assertIsNotNone(admin_user.user.groups.get(name='administrator'))
 		self.assertEqual(str(admin_user), 'temporary')
+
+
+	def test_base_user_group(self):
+		"""Tests if the group for base user is assigned correctly
+		"""
+		self.assertIsNotNone(self.base_user.user.groups.get(name='usuario_base'))		
 
 
 
