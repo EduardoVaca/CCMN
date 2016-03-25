ee7a49da37e3f82417c269265ca6df9f8774fbcc
EduardoVaca <vacalalo@gmail.com>
2016-03-25 15:08:47 -0600

    Added template for adding new borrow

--- book_borrows/urls.py
+++ book_borrows/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^administrador/borrow/$', views.borrow_list, name='borrow_list'),
+	url(r'^administrador/borrow/add/$', views.borrow_add, name='borrow_add'),
 ]
--- book_borrows/views.py
+++ book_borrows/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 
-from . models import Borrow
+from .models import Borrow
+from library.models import Book
+from users.models import BaseUser
 
 
 def in_admin_group(user):
@@ -19,4 +21,14 @@ def borrow_list(request):
 	}
 	return render(request, 'administrador/borrow_list.html', context)
 
+
+@user_passes_test(in_admin_group, login_url = 'admin_users:authentication')
+def borrow_add(request):
+	books = Book.objects.filter(book_status='AV')
+	users = BaseUser.objects.all()
+	context = {
+		'books': books,
+		'users': users,
+	}
+	return render(request, 'administrador/borrow_add.html', context)
 
