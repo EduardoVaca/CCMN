8deba63d1a5707986ae8dacb9db5ee9e73bcd9dc
EduardoVaca <vacalalo@gmail.com>
2016-03-25 14:30:26 -0600

    Added borrow_list view

--- book_borrows/urls.py
+++ book_borrows/urls.py
@@ -1,1 +1,8 @@
-urls.py
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+	url(r'^administrador/borrow/$', views.borrow_list, name='borrow_list'),
+]
--- book_borrows/views.py
+++ book_borrows/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
 
-# Create your views here.
+from . models import Borrow
+
+
+def in_admin_group(user):
+    """Use with a ``user_passes_test`` decorator to restrict access to 
+    authenticated users who are not in the "administrator" group."""
+    return user.is_authenticated() and (user.is_superuser or user.groups.filter(name='administrator').exists())
+
+
+@user_passes_test(in_admin_group, login_url = 'admin_users:authentication')
+def borrow_list(request):
+	borrows = Borrow.objects.all()
+	context = {
+		'borrows': borrows,
+	}
+	return render(request, 'administrador/borrow_list.html', context)
+
+
