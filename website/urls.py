f483acb3d630ddd6d5eba84f1e5ca9b8dfda0707
EduardoVaca <vacalalo@gmail.com>
2016-04-11 16:20:39 -0500

    Added redirect in sala de lectura

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [	
 	url(r'^login/$', views.login, name='login'),
+	url(r'^lectura/$', views.book_list, name='book_list'),
 	url(r'^$', views.index, name='index'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+
+from users.models import BaseUser
+
+def in_base_user_group(user):
+    """Use with a ``user_passes_test`` decorator to restrict access to 
+    authenticated users who are not in the "administrator" group."""
+    return user.is_authenticated() and user.groups.filter(name='usuario_base').exists()
 
 
 def index(request):
@@ -9,3 +18,9 @@ def index(request):
 def login(request):
 	context = {}
 	return render(request, 'website/login.html', context)
+
+
+@user_passes_test(in_base_user_group, login_url = 'website:login')
+def book_list(request): 
+	context = {}
+	return render(request, 'website/book_list.html', context)
