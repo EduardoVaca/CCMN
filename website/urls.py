8dafb09d7df56eb8359eb07cde91709b381a3ba9
EduardoVaca <vacalalo@gmail.com>
2016-04-11 16:31:50 -0500

    Added login functionality

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [	
-	url(r'^login/$', views.login, name='login'),
+	url(r'^login/$', views.user_login, name='login'),
 	url(r'^lectura/$', views.book_list, name='book_list'),
 	url(r'^$', views.index, name='index'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 from users.models import BaseUser
 
@@ -15,7 +17,18 @@ def index(request):
 	return render(request, 'website/index.html', context)
 
 
-def login(request):
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+
+		user = authenticate(username=username, password=password)
+		if user:	
+			if user.groups.filter(name='usuario_base').exists():
+				login(request, user)
+				return redirect('website:book_list')
+			else:
+				messages.error(request, 'La cuenta de usuario debe existir para poder ingresar')
 	context = {}
 	return render(request, 'website/login.html', context)
 
