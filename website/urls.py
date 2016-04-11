8173e8d06dfe214245321bf82e6ab8fc4192cc58
EduardoVaca <vacalalo@gmail.com>
2016-04-11 16:35:10 -0500

    Logour ready

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [	
 	url(r'^login/$', views.user_login, name='login'),
+	url(r'^logout/$', auth_views.logout, {'next_page': 'website:login'}, name='logout'),
 	url(r'^lectura/$', views.book_list, name='book_list'),
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='index'),	
 ]
