b70964265bb4453bf81020fd0d32d7562b7ca7cb
EduardoVaca <vacalalo@gmail.com>
2016-04-17 18:38:55 -0500

    Added filter by book name... missing by authors and categories

--- website/views.py
+++ website/views.py
@@ -37,7 +37,19 @@ def user_login(request):
 @user_passes_test(in_base_user_group, login_url = 'website:login')
 def book_list(request):
 	books = Book.objects.all().order_by('name') 
+	search = '¿Qué es lo que buscas?'
+	if request.method == 'GET':
+		search_text = request.GET.get('search-text', None)
+		search_class = request.GET.get('search-class', None)
+		search = search_text		
+		if search_class == 'Book':
+			books = Book.objects.filter(name__icontains=search_text).order_by('name')	
+		elif search_class == 'Author':
+			books = Book.objects.filter(authors__icontains=search_text).order_by('name')
+		elif search_class == 'Category':
+			books = Book.objects.filter(categories__icontains=search_text).order_by('name')
 	context = {
 		'books': books,
+		'search': search,
 	}
-	return render(request, 'website/book_list.html', context)
+	return render(request, 'website/book_list.html', context)	
