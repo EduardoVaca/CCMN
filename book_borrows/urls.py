8569f2d39cb55356a7c6a1e932f9f69c81ac210e
EduardoVaca <vacalalo@gmail.com>
2016-04-18 12:25:15 -0500

    Added rennovation function in admin borrow moduke

--- book_borrows/urls.py
+++ book_borrows/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 
 
-urlpatterns = [
-	url(r'^administrador/borrow/$', views.borrow_list, name='borrow_list'),
+urlpatterns = [	
 	url(r'^administrador/borrow/add/$', views.borrow_add, name='borrow_add'),
 	url(r'^administrador/borrow/return/(?P<pk>[0-9]+)/$', views.borrow_return, name='borrow_return'),
+	url(r'^administrador/borrow/rennovation/(?P<pk>[0-9]+)/$', views.borrow_rennovation, name='borrow_rennovation'),
+	url(r'^administrador/borrow/$', views.borrow_list, name='borrow_list'),
 ]
--- book_borrows/views.py
+++ book_borrows/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import date, datetime, timedelta
 
 from django.utils.dateparse import parse_datetime
 from django.utils import timezone
@@ -62,3 +62,25 @@ def borrow_return(request, pk):
 	messages.info(request, 'El libro se ha marcado como regresado')	
 	return redirect('book_borrows:borrow_list')
 
+
+@user_passes_test(in_admin_group, login_url = 'admin_users:authentication')
+def borrow_rennovation(request, pk):
+	borrow = get_object_or_404(Borrow, pk=pk)
+	begin_date = borrow.start_date
+	end_date = borrow.end_date
+	rennovations = borrow.rennovations
+	days_diff = (end_date.date() - begin_date.date()).days 
+	if days_diff < 2 and days_diff > 0:
+		if rennovations < 3:
+			if borrow.status == 'BO':				
+				rennovations += 1
+				Borrow.objects.filter(pk=pk).update(end_date=datetime.now()+timedelta(days=3))
+				Borrow.objects.filter(pk=pk).update(rennovations=rennovations)
+				messages.success(request, 'El libro se ha renovado con éxito, verifique nueva fecha de devolución')
+			else:
+				messages.error(request, 'El libro debe estar en estado de préstamo para ser renovado')
+		else:
+			messages.error(request, 'No se pueden hacer mas de tres renovaciones')
+	else:
+		messages.error(request, 'Lo sentimos, es muy pronto para renovar este libro')
+	return redirect('book_borrows:borrow_list')			
